e9c6e308db14f8b59d8306941fb64f8897877ba3
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-03-10 03:34:20 +0100

    removed react hook form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import { IconContext } from "react-icons";
 import { IoIosIceCream } from "react-icons/io";
-import { useForm } from "react-hook-form";
-import validator from "validator";
-import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
 
 const variants = {
   vanilla: "Vanille",
@@ -14,38 +11,14 @@ const variants = {
 };
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors, touchedFields, isDirty, dirtyFields, isSubmitting },
-  } = useForm({
-    mode: "onBlur",
-    defaultValues: {
-      variant: null,
-      name: "",
-      email: "",
-      numBalls: null,
-      extraCream: false,
-    },
-  });
-
-  const variant = watch("variant");
-  const name = watch("name");
-  console.log(errors);
-
-  const onSubmit = (data) => {
-    const message = `Hier ist Dein Eis, ${data.name}: ${data.numBalls} Kugeln ${
-      variants[data.variant]
-    }${
-      data.extraCream ? " mit extra viel Sahne." : "."
-    } Wir schicken die Rechnung an ${data.email}`;
+  const onSubmit = () => {
+    const message = `Hier ist Dein Eis, NAME: NN Kugeln SORTE MIT/OHNE extra viel Sahne. Wir schicken die Rechnung an EMAIL`;
     alert(JSON.stringify(message));
   };
 
   return (
     <div className="container">
-      <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <form autoComplete="off" onSubmit={onSubmit}>
         <h1 className="title">
           neuefische
           <IconContext.Provider value={{ className: "icon" }}>
@@ -77,35 +50,17 @@ function App() {
           autoComplete="new-password"
           type="text"
           placeholder="Name"
-          {...register("name", {
-            required: { value: true, message: "Bitte gib Deinen Namen ein." },
-            minLength: { value: 2, message: "Das soll ein Name sein?" },
-            maxLength: {
-              value: 20,
-              message: "Das kann sich doch keiner merken!",
-            },
-          })}
         />
-        <span className="error-message">{errors.name?.message}</span>
+        <span className="error-message">Fehler</span>
         <label className="caption" htmlFor="email">
           Wie erreichen wir Dich?
         </label>
-        <input
-          autoComplete="new-password"
-          type="text"
-          placeholder="E-Mail"
-          {...register("email", {
-            required: { value: true, message: "Bitte gib Deine E-Mail ein!" },
-            validate: (value) => {
-              return validator.isEmail(value) || "Das ist keine gültige E-Mail";
-            },
-          })}
-        />
-        <span className="error-message">{errors.email?.message}</span>
+        <input autoComplete="new-password" type="text" placeholder="E-Mail" />
+        <span className="error-message">Fehler</span>
         <label className="caption" htmlFor="variant">
           Welche Sorte willst Du?
         </label>
-        <select name="variant" id="variant" {...register("variant")}>
+        <select name="variant" id="variant">
           {Object.entries(variants).map(([value, text], index) => {
             return (
               <option key={index} value={value}>
@@ -114,34 +69,23 @@ function App() {
             );
           })}
         </select>
-        <span className="error-message">{errors.variant?.message}</span>
+        <span className="error-message">Fehler</span>
 
         <label className="caption" htmlFor="email">
           Wieviel Bällchen dürfen es denn sein?
         </label>
-        <input
-          type="number"
-          {...register("numBalls", {
-            min: { value: 1, message: "Du wolltest doch ein Eis oder nicht?" },
-            max: { value: 5, message: "Davon wirst Du zu viel zu dick!" },
-          })}
-        />
-        <span className="error-message">{errors.numBalls?.message}</span>
+        <input type="number" />
+        <span className="error-message">Fehler</span>
 
         <label htmlFor="newsletter">
-          <input id="newsletter" type="checkbox" {...register("extraCream")} />
+          <input id="newsletter" type="checkbox" />
           Extra viel Sahne!
         </label>
 
-        <button disabled={!isDirty} type="submit">
-          Gib mir mein Eis!
-        </button>
+        <button type="submit">Gib mir mein Eis!</button>
       </form>
-      {variant ? (
-        <p>
-          Gute Wahl {name} - {variants[variant]} ist lecker
-        </p>
-      ) : undefined}
+
+      <p>Gute Wahl NAME - SORTE ist lecker</p>
     </div>
   );
 }
